760b80779df17ae129fca81ef198a408390aacad
SofiDevO <angiemeow89@gmail.com>
2025-04-13 19:49:55 -0600

    🚧 Refactorizando estructura y estilos

--- src/components/atoms/BlogPost/BlogPost.jsx
+++ src/components/atoms/BlogPost/BlogPost.jsx
@@ -8,9 +8,20 @@ const BlogPost = () => {
 console.log(userData.blog)
   return (
     <>
+      <a
+        href={`${userData.blog}/blog/${firstPost.slug}`}
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+      >
+        <h2>{firstPost.title}</h2>
+      </a>
       {firstPost ? (
-        <a href={`${userData.blog}/blog/${firstPost.slug}`} target="_blank" rel="noopener noreferrer nofollow" className="flex flex-col p-2 bg-btn rounded-2xl mb-3.5 max-h-[30dvh] aspect-video">
-
+        <a
+          href={`${userData.blog}/blog/${firstPost.slug}`}
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+          className="w-full flex flex-col p-2 bg-btn rounded-2xl mb-3.5 min-h-[30dvh]  aspect-video"
+        >
           <div className="rounded-2xl overflow-hidden group">
             <img
               alt={firstPost.featuredImage?.node.altText || "Post image"}
@@ -20,10 +31,7 @@ console.log(userData.blog)
               }
               className="w-full h-full object-cover group-hover:scale-[1.05]  group-hover:saturate-50 transition-transform duration-500 ease-in-out"
             />
-
           </div>
-
-
         </a>
       ) : (
         <p>No hay posts disponibles.</p>
